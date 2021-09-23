eef6eb8c4b94b4df26a3c7629f19b876f5751b44
Joshua Stoutenburg <jehoshua02@gmail.com>
2021-09-23 14:31:22 -0600

    Generate module for autocomplete section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'material', loadChildren: () => import('./routes/material/material.module').then(m => m.MaterialModule) }];
+const routes: Routes = [
+  { path: 'material', loadChildren: () => import('./routes/material/material.module').then(m => m.MaterialModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
