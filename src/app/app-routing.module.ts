1041eff39a3960499c69d504eb51621245c3b0d6
Joshua Stoutenburg <jehoshua02@gmail.com>
2021-09-23 14:21:03 -0600

    material works!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'material', loadChildren: () => import('./routes/material/material.module').then(m => m.MaterialModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
