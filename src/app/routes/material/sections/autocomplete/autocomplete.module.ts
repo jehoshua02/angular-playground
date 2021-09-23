514ff262e5c7f1cb7cc2baa862d566a4509eaeda
Joshua Stoutenburg <jehoshua02@gmail.com>
2021-09-23 14:34:44 -0600

    npx ng g c routes/material/sections/autocomplete

--- src/app/routes/material/sections/autocomplete/autocomplete.module.ts
+++ src/app/routes/material/sections/autocomplete/autocomplete.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AutocompleteComponent } from './autocomplete.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AutocompleteComponent
+  ],
   imports: [
     CommonModule
   ]
